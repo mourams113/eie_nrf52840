30d4df1732615f86e6b36d24684897bbcf2cd51c
mourams113 <mourams113@gmail.com>
2025-10-16 20:16:05 -0600

    One LED Flashing Faster

--- app/src/main.c
+++ app/src/main.c
@@ -31,6 +31,9 @@ int main(void)
 
     while (1){
         gpio_pin_toggle_dt(&led0);
+        k_msleep(500);
+
+        gpio_pin_toggle_dt(&led0);
         gpio_pin_toggle_dt(&led1);
         gpio_pin_toggle_dt(&led2);
         gpio_pin_toggle_dt(&led3);
